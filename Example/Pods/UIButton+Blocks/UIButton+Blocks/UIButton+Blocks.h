0f8c15b68eccd4ab64fc8c22c494508aff530b0c
jiang.zijia@immomo.com <jiang.zijia@immomo.com>
2017-05-03 13:43:36 +0800

    add .xcworkspace

---
+++ Example/Pods/UIButton+Blocks/UIButton+Blocks/UIButton+Blocks.h
@@ -0,0 +1,21 @@
+//
+//  UIButton+Block.h
+//  DEMO7
+//
+//  Created by AcFun on 16/5/27.
+//  Copyright © 2016年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+typedef void (^ActionBlock)(UIButton* sender);
+
+@interface UIButton (Blocks)
+
+-(void)handleClickEvent:(UIControlEvents)aEvent withClickBlick:(ActionBlock)buttonClickEvent;
+
+/*
+ default UIControlEventTouchUpInside
+ */
+-(void)handleClickWithClickBlick:(ActionBlock)buttonClickEvent;
+
+@end
---
+++ Example/Pods/UIButton+Blocks/UIButton+Blocks/UIButton+Blocks.m
@@ -0,0 +1,40 @@
+//
+//  UIButton+Block.m
+//  DEMO7
+//
+//  Created by AcFun on 16/5/27.
+//  Copyright © 2016年 J. All rights reserved.
+//
+
+#import "UIButton+Blocks.h"
+#import <objc/runtime.h>
+static char *overViewKey;
+
+@implementation UIButton (Blocks)
+
+
+-(void)handleClickWithClickBlick:(ActionBlock)buttonClickEvent{
+    
+    [self handleClickEvent:UIControlEventTouchUpInside withClickBlick:buttonClickEvent];
+
+}
+
+-(void)handleClickEvent:(UIControlEvents)aEvent withClickBlick:(ActionBlock)buttonClickEvent
+{
+    objc_setAssociatedObject(self, &overViewKey, buttonClickEvent, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    [self addTarget:self action:@selector(buttonClick) forControlEvents:aEvent];
+}
+
+-(void)buttonClick
+{
+    ActionBlock blockClick = objc_getAssociatedObject(self, &overViewKey);
+    if (blockClick != nil)
+    {
+        blockClick(self);
+    }
+}
+
+
+
+
+@end
